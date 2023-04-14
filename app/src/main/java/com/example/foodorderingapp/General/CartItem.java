df241df3d6ca40ea122b98c9a3be96ad9956cc4c
niilotallberg <tallbergniilo@gmail.com>
2023-04-14 13:27:50 +0300

    Created a whole "CartItem"-class

---
+++ app/src/main/java/com/example/foodorderingapp/General/CartItem.java
@@ -0,0 +1,27 @@
+package com.example.foodorderingapp.General;
+
+public class CartItem {
+    private String itemId;
+    private int quantity;
+
+    public CartItem(String itemId, int quantity) {
+        this.itemId = itemId;
+        this.quantity = quantity;
+    }
+
+    public String getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(String itemId) {
+        this.itemId = itemId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
