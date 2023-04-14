bee5df1190607c6558be310bff452cfda922a6eb
niilotallberg <tallbergniilo@gmail.com>
2023-04-14 13:26:46 +0300

    Created a whole "User"-class

---
+++ app/src/main/java/com/example/foodorderingapp/General/User.java
@@ -0,0 +1,57 @@
+package com.example.foodorderingapp.General;
+
+public class User {
+    private String username;
+    private String email;
+    private String password;
+    private String address;
+    private String profilePicture;
+
+    public User(String username, String email, String password, String address, String profilePicture) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.address = address;
+        this.profilePicture = profilePicture;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getProfilePicture() {
+        return profilePicture;
+    }
+
+    public void setProfilePicture(String profilePicture) {
+        this.profilePicture = profilePicture;
+    }
+}
