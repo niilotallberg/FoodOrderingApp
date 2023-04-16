edc4aef72865b61ae4f0aecb087859b981abf97c
niilotallberg <tallbergniilo@gmail.com>
2023-04-16 13:12:15 +0300

    added a onClickListener for Logout

--- app/src/main/java/com/example/foodorderingapp/Activity/ProfileSettingsActivity.java
+++ app/src/main/java/com/example/foodorderingapp/Activity/ProfileSettingsActivity.java
@@ -2,15 +2,31 @@ package com.example.foodorderingapp.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.foodorderingapp.R;
 
 public class ProfileSettingsActivity extends AppCompatActivity {
 
+    private Button btnLogout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_settings);
+
+        btnLogout = findViewById(R.id.logOutButton);
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ProfileSettingsActivity.this, LoginActivity.class);
+                // TODO Ulos-kirjautumis toiminnallisuudet
+                startActivity(intent);
+            }
+        });
     }
 }
