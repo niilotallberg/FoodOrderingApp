3878e47d37e83ff10bd614b6fa7dbde228ec061f
niilotallberg <tallbergniilo@gmail.com>
2023-04-16 15:45:03 +0300

    Added functionality to show existing profile  information

--- app/src/main/java/com/example/foodorderingapp/Activity/ProfileSettingsActivity.java
+++ app/src/main/java/com/example/foodorderingapp/Activity/ProfileSettingsActivity.java
@@ -7,11 +7,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.foodorderingapp.General.User;
+import com.example.foodorderingapp.General.UserManager;
 import com.example.foodorderingapp.R;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class ProfileSettingsActivity extends AppCompatActivity {
 
     private Button btnLogout;
+    private Button btnSaveChanges;
+    private TextInputEditText etUsername, etEmail, etPassword, etAddress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,20 @@ public class ProfileSettingsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile_settings);
 
         btnLogout = findViewById(R.id.logOutButton);
+        btnSaveChanges = findViewById(R.id.saveButton);
+        etUsername = findViewById(R.id.editUsername);
+        etEmail = findViewById(R.id.editEmail);
+        etPassword = findViewById(R.id.editPassword);
+        etAddress = findViewById(R.id.editAddress);
+
+        // Set the fields with the known user information
+        User currentUser = UserManager.getInstance().getCurrentUser();
+        if (currentUser != null) {
+            etUsername.setText(currentUser.getUsername());
+            etEmail.setText(currentUser.getEmail());
+            etPassword.setText(currentUser.getPassword());
+            etAddress.setText(currentUser.getAddress());
+        }
 
         btnLogout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,6 +45,20 @@ public class ProfileSettingsActivity extends AppCompatActivity {
                 Intent intent = new Intent(ProfileSettingsActivity.this, LoginActivity.class);
                 // TODO Ulos-kirjautumis toiminnallisuudet
                 startActivity(intent);
+            }
+        });
+
+        btnSaveChanges.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Save the changes made by the user
+                UserManager.getInstance().updateCurrentUser(
+                        etUsername.getText().toString(),
+                        etEmail.getText().toString(),
+                        etPassword.getText().toString(),
+                        etAddress.getText().toString()
+                );
+                // Save the updated user information to your data source, e.g., database, shared preferences, etc.
             }
         });
     }
