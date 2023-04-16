210c2cff6762e3093e0a20f78700ffd63cfe8b91
niilotallberg <tallbergniilo@gmail.com>
2023-04-16 13:50:04 +0300

    Created a singleton userManager-class to store one user at a time

---
+++ app/src/main/java/com/example/foodorderingapp/General/UserManager.java
@@ -0,0 +1,24 @@
+package com.example.foodorderingapp.General;
+
+public class UserManager {
+    private static UserManager instance;
+    private User currentUser;
+
+    private UserManager() {
+    }
+
+    public static UserManager getInstance() {
+        if (instance == null) {
+            instance = new UserManager();
+        }
+        return instance;
+    }
+
+    public User getCurrentUser() {
+        return currentUser;
+    }
+
+    public void setCurrentUser(User currentUser) {
+        this.currentUser = currentUser;
+    }
+}
