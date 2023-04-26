d9b9f1b511266b34ae0448b9e56736c064ccee52
niilotallberg <tallbergniilo@gmail.com>
2023-04-26 14:25:44 +0300

    added CoordinatorLayout to DetailCategoryActivity

--- app/src/main/java/com/example/foodorderingapp/Activity/DetailCategoryActivity.java
+++ app/src/main/java/com/example/foodorderingapp/Activity/DetailCategoryActivity.java
@@ -5,10 +5,15 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
 import com.example.foodorderingapp.Adapter.DetailCategoryAdapter;
 import com.example.foodorderingapp.General.FoodDomain;
 import com.example.foodorderingapp.General.FoodStorage;
 import com.example.foodorderingapp.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import java.util.List;
 
 public class DetailCategoryActivity extends AppCompatActivity {
@@ -29,5 +34,49 @@ public class DetailCategoryActivity extends AppCompatActivity {
         List<FoodDomain> foodList = foodStorage.getFoodListByCategory(category);
         adapter = new DetailCategoryAdapter(foodList);
         recyclerView.setAdapter(adapter);
+
+        bottomNavigation();
+    }
+
+    private void bottomNavigation() {
+        FloatingActionButton floatingActionButton = findViewById(R.id.btnCartView);
+        LinearLayout homeBtn = findViewById(R.id.homeBtn);
+        LinearLayout profileBtn = findViewById(R.id.profileBtn);
+        LinearLayout favoritesBtn = findViewById(R.id.favoritesBtn);
+        LinearLayout chatBtn = findViewById(R.id.chatBtn);
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(DetailCategoryActivity.this, CartListActivity.class));
+            }
+        });
+        homeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(DetailCategoryActivity.this, HomePageActivity.class));
+            }
+        });
+
+        profileBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(DetailCategoryActivity.this, ProfileSettingsActivity.class));
+            }
+        });
+
+        favoritesBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(DetailCategoryActivity.this, FavoritesActivity.class));
+            }
+        });
+
+        chatBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(DetailCategoryActivity.this, FaqActivity.class));
+            }
+        });
     }
 }
